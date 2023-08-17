4ce65cf307c14b7b88f6b1d7bac5f1293b2b6554
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-17 16:22:42 -0500

    variable example.

---
+++ Object and Data Strucure basics/1. Python Objects and Data Structures/variables.py
@@ -0,0 +1,12 @@
+# names cannot start with a number.
+# No spaces  and cannot use special characters.
+# Names should be lowercase.
+# Avoid using special meaning words like str.
+x = 10
+y = x + x
+print(x)
+print(y)
+# can reassign variables later on
+
+x = "Ten"
+print(x)
