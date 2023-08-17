a89379693c8431b275d1ef15c65c9b70b30ee420
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-17 16:22:03 -0500

    How to index.

---
+++ Object and Data Strucure basics/1. Python Objects and Data Structures/Strings/indexing.py
@@ -0,0 +1,9 @@
+x = "Hello World"
+print(x[0])  # indexing starts at 0.
+print(x[-1])  # can go backwards
+print(x[1:5])  # slicing gos from index 1 up to but not including 5
+print(x[:5])  # slicing gos from index 0 up to but not including 5
+y = "This is my string"
+print(y[:10:2])  # slicing gos from index 0 up to but not including 10 every 2
+print(y[::-1])  # reverses order from beginning to end and prints it
+
