32488ed7cc2d87bcf1e1aeb81c87da017148a969
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-21 23:36:54 -0500

    FizzBuzz was easy.

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+numbers = range(0, 100)
+for nums in numbers:
+    if nums %3==0 and nums %5==0:
+        print("FizzBuzz")
+    elif nums %3==0:
+        print("Fizz")
+    elif nums %5==0:
+        print("Buzz")
+    else:
+        print(nums)
