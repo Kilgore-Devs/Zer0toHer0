0f40aff937bcfe127ddf7b27b6222fab384ccd7b
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-25 23:54:22 -0500

    bools and fxs

---
+++ 4. Methods and Functions/simple boolean.py
@@ -0,0 +1,19 @@
+def myfunc(x):  # defines myfunc and takes in x
+
+    if x == True:  # print and return if true
+        print("I like Python.")
+        return "I like Python."
+
+    elif x == False:  # print and return if false
+        print("I do not like Python. :(")
+        return "I do not like Python. :("
+
+
+y = input("True or False:\nPython is amazing?  ")  # sets x using input
+if y == "True" or "true":
+    x = True
+elif:
+    y == "False" or "false":
+    x = False
+
+myfunc(x)  # runs function using the result of if statement
