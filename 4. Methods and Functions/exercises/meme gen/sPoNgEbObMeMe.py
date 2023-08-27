b892d126a15cee00e255543988a59195cec835c2
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-27 00:09:34 -0500

    start of spongebob meme generator

---
+++ 4. Methods and Functions/exercises/meme gen/sPoNgEbObMeMe.py
@@ -0,0 +1,13 @@
+
+def spongebob(anything):
+    result = ""
+    for i, letters in enumerate(anything):
+        if i % 2 == 0:
+            result += letters.lower()
+        else:
+            result += letters.upper()
+    return result
+
+
+meme = input("Input a phrase here:   ")
+print(spongebob(meme))
