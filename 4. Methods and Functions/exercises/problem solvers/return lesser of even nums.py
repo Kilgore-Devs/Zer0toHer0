3a2aaaa276c044933a36d15b3e223328c6758882
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-28 21:31:47 -0500

    function that takes in 2 numbers and returns one if conditions are met

--- 4. Methods and Functions/exercises/problem solvers/return lesser of even nums.py
+++ 4. Methods and Functions/exercises/problem solvers/return lesser of even nums.py
@@ -1,11 +1,14 @@
 """Write a function that returns the lesser of two given numbers if both numbers are even,
 but returns the greater if one or both numbers are odd"""
+
+
 def lesser_of_evens(a, b):
     if a % 2 == 0 and b % 2 == 0:
         print(a, b)
         return min(a, b)
     else:
-        print(max(a,b))
+        print(max(a, b))
         return max(a, b)
+
 
 lesser_of_evens(777, 888)
