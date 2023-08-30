5cc89c3d85284a2c1c5843325fb8bc176cc2f65b
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-08-30 00:14:07 -0500

    finished up with examples of using lambda functions

--- 4. Methods and Functions/lambda/everything_lambda.py
+++ 4. Methods and Functions/lambda/everything_lambda.py
@@ -1,6 +1,7 @@
 def square(n):
     return n**2
 
+
 nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 nums2 = reversed(nums)  # reverses order of "nums"
 map(square, nums)  # pass in function you want to map to every item in list
@@ -15,12 +16,38 @@ for item in map(square, nums):
 
 print(list(map(square, nums)))  # maps square to items in nums, prints as a list
 
+
 def splicer(words):
-    if len(words) % 2 == 0: # if len of 'words' is even
+    if len(words) % 2 == 0:  # if len of 'words' is even
         return "Even"  # return even else return first letter
     else:
         return words[0]
 
+
 cars = "M3", "Accord", "Viper", "Challenger"
 
-print(list(map(splicer, cars)))
+print(list(map(splicer, cars)))  # dont add () after fx name, its an arg,  not calling the fx
+
+
+def check_even(number):
+    return number % 2 == 0
+
+
+thenums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(filter(check_even, thenums))
+
+for n in filter(check_even, thenums):
+    print(n)
+
+print(list(filter(check_even, thenums)))
+# lambda uses
+square  = lambda n: n ** 2  # converting square fx into lambda
+print(square(99))
+
+print(list(map(lambda num:num**2, nums)))
+print(list(filter(lambda num:num % 2 == 0, thenums)))  # checks for even numbers in thenums list
+
+print(list(map(lambda names:names[0], cars)))  # prints first letter of each item in cars list, via filtering
+print(list(map(lambda names:names[::-1], cars)))  # reverses
+
