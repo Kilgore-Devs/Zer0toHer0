25e7dc51d7f6bad699600e4c709e167e445434ee
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-09-03 22:30:20 -0500

    added comments and print statement

--- 4. Methods and Functions/exercises/sum fx.py
+++ 4. Methods and Functions/exercises/sum fx.py
@@ -1,2 +1,6 @@
-def my_func(a, b):
-    return a+b
+def my_func(a, b):  # my_func() in takes two numbers
+    print(a + b)  # returns those two numbers added together
+    return a + b
+
+
+my_func(50, 150)  # passing 50 and 150 into my_func()
