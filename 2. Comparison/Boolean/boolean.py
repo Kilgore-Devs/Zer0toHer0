8246de55031c18424e66f1feeddaa7d791e23861
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-09-26 13:27:43 -0500

    added comments

--- 2. Comparison/Boolean/boolean.py
+++ 2. Comparison/Boolean/boolean.py
@@ -3,7 +3,7 @@ print(type(False))  # is a boolean
 print(type("false"))  # is a string
 # check True or False
 print(bool(1 > 2))  # checks if true or false
-print(bool(1 = 1))  # checks if not equal to
+print(bool(1 != 1))  # checks if not equal to
 name = None  # place holder for "name" for later use
 print(type(name))  # "NoneType"
 print(2.0 == 2)  # is equal to?
