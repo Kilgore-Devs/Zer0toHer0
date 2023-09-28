4a70033e14ed4a487528653ec5d7d6a63008a658
Kilgore_Devs <Dr.Dev.Kilgore@gmail.com>
2023-09-27 21:30:16 -0500

    added statement inside print

--- 2. Comparison/Boolean/boolean.py
+++ 2. Comparison/Boolean/boolean.py
@@ -21,4 +21,4 @@ print(not 1 == 1)  # opposite
 counter = 0  # set counter
 while counter <= 100:  # while counter is less than 100 do the loop
     counter += 1  # increment counter by 1
-    print(counter)  # print the counter
+    print("this is my count: " + str(counter))  # print the counter
